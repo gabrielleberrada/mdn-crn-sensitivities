b6aac4a88cdc71a85369638752cce7d759862132
gabrielleblj <gabrielleblj@gmail.com>
2022-09-30 15:35:20 +0200

    SSA simulations + generating data for training

---
+++ generate_data.py
@@ -0,0 +1,131 @@
+import torch
+import numpy as np
+import math
+import scipy.stats.qmc as qmc
+import time
+import concurrent.futures
+import test
+import simulation
+
+
+class CRN_Dataset:
+
+    def __init__(self, crn, sampling_times, n_trajectories=10**3, ind_specy=0):
+        """
+        Initializes parameters to build our dataset.
+
+        Inputs: 'crn': Chemical Reaction Network to work on.
+                'sampling_times': Times to sample.
+                'n_trajectories': Number of trajectories to compute. 
+                                Can also be defined when calling the 'generate_data' function.
+                'ind_specy': Index of the specy of interest. 
+                            The distribution generated will be the one of that specy.
+        """
+        self.crn = crn
+        self.n_params = crn.n_params
+        self.n_species = crn.n_species
+        self.sampling_times = sampling_times
+        self.n_trajectories = n_trajectories
+        self.ind_specy = ind_specy
+    
+    def samples_probs(self, params):
+        """
+        Runs n_trajectories of the SSA simulation for the parameters in input and deducts the corresponding distribution
+        for the specy indexed by 'ind_specy'.
+
+        Inputs: 'params': the parameters of the propensities.
+
+        Outputs:
+            'samples': list of the distributions for the corresponding specy at times time_samples.
+                        This list begins with time and parameters: [t, param1, ..., paramK, distr]
+                        in order to keep parameters linked to the distribution.
+            'max_value': maximum value reached during simulations + number of parameters + 1 (for time).
+                        Will be useful to standardize each data length to turn the data list into a tensor.
+        """
+        res = []
+        start_ = time.time()
+        for _ in range(self.n_trajectories):
+            _, samples = self.crn.simulation(np.zeros(self.n_species, dtype=np.float32), 
+                                    params, self.sampling_times, self.sampling_times[-1])
+            res.append(samples)
+        end_ = time.time()
+        print('Done, time needed:', end_ - start_)
+        res = np.array(res)
+        max_value = int(np.max(res))
+        # Counts of events for each specy
+        distr = np.empty((int(max_value) + 1, len(self.sampling_times), self.crn.n_species))
+        for i in range(int(max_value)+1):
+            distr[i,:,:] = np.count_nonzero((res == i), axis=0)
+        # final output
+        samples = []
+        for i, t in enumerate(self.sampling_times):
+            sample = [t] + list(params) + list(distr[:,i, self.ind_specy])
+            samples.append(sample)
+        # + 1 for time
+        return samples, max_value + self.n_params + 1
+
+    def set_length(self, onedim_tab, length):
+        """
+        Inputs: 'onedim_tab': array to extend. In our case, 1D array.
+                'length': wanted length of array.
+
+        Output: An array of length 'length' equal to 'onedim_tab' array completed by zeros at the end.
+        """
+        return onedim_tab + [0] * max(length - len(onedim_tab), 0)
+
+    def generate_data(self, data_length, n_trajectories=10**3, sobol_length=10, ind_specy=0):
+        """
+        Generates a training, validation or test dataset.
+        We use multiprocessing to run simulations in parallel to compute faster.
+        Parameters are generated from the Sobol Sequence.
+
+        Inputs: 'data_length': length of data expected in outputs.
+                'n_trajectories': number of trajectories to compute to build the distribution.
+                'sobol_length': upper bound of the hypercube [0, length]^N in which each set of parameters lie.
+                'ind_specy': index of the specy of which to compute the distribution.
+        
+        Output: A tuple of tensors '(X, y)'. 
+                Each entry of 'X' is an input to the neural network of the form '[t, params...]'
+                The corresponding entry of 'y' is the expected probability distribution for these parameters.
+        """
+        self.n_trajectories = n_trajectories
+        self.ind_specy = ind_specy
+        n_params = self.crn.n_params
+        start = time.time()
+        sobol = qmc.Sobol(n_params, scramble=False)
+        # to avoid 0.
+        sobol.random()
+        # generating parameters
+        # sobol sequence requires a power of 2
+        n_elts = 2**math.ceil(np.log2(data_length/len(self.sampling_times)))
+        params = sobol.random(n_elts)*sobol_length # array of n_elts of set of parameters, each set of length n_params
+        # using multithreading to process faster
+        with concurrent.futures.ProcessPoolExecutor() as executor:
+            res = executor.map(self.samples_probs, params)
+        print('Simulations done.')
+        distributions = []
+        max_value = 0
+        for distrs, value in res:
+            for distr in distrs:
+                distributions.append(distr)
+            if value > max_value:
+                max_value = value
+        # shaping distributions to turn it into an array
+        distributions = list(map(lambda d: self.set_length(d, max_value + 1), distributions))
+        distributions = np.array(distributions)
+        # split 'distributions' into input data and output data
+        X = torch.tensor(distributions[:, :1+n_params].copy())
+        y = torch.tensor(distributions[:, 1+n_params:].copy()/n_trajectories)
+        end=time.time()
+        print('Total time: ', end-start)
+        return X, y
+
+# # Testing
+
+# if __name__ == '__main__':
+
+#     stoichiometric_mat = np.array([1]).reshape(1,1)
+#     crn = simulation.CRN(stoichiometric_mat, np.array([test.lambda1]), 1)
+#     dataset = CRN_Dataset(crn, np.array([5, 10, 25]))
+#     X, y = dataset.generate_data(10)
+#     print(X,'\n', y)
--- simulation.py
+++ simulation.py
@@ -1,42 +1,48 @@
-from re import L
 import numpy as np
-import torch
 import random
 
 
 class CRN:
-    def __init__(self, stoichiometric_mat, propensities):
-        self.n_species, self.n_reactions = np.shape(stoichiometric_mat)
+    def __init__(self, stoichiometric_mat, propensities, n_params):
         self.stoichiometric_mat = stoichiometric_mat
+        self.n_species, self.n_reactions = np.shape(stoichiometric_mat)
         self.propensities = propensities
+        self.n_params = n_params
+
+    def parameters_propensities(self, params):
+        # associates to each function its parameter
+        lambdas = self.propensities
+        set_parameters = np.vectorize(lambda f, params: (lambda x: f(params, x)), excluded=[1])
+        return set_parameters(lambdas, params)
+
+    def simulation(self, init_state, params, sampling_times, tf):
+        lambdas = self.parameters_propensities(params)
+        ssa = SSA(init_state, tf, sampling_times, lambdas, self.n_species, self.n_reactions, self.stoichiometric_mat)
+        return ssa.step()
+
 
 
 class SSA:
-    def __init__(self, x0, tf, crn, sampling_times, parameters):
+    def __init__(self, x0, tf, sampling_times, propensities, n_species, n_reactions, stoich_mat):
         self.initial_state = x0 # necessary?
         self.final_time = tf
-        self.CRN = crn
+        self.n_species = n_species
+        self.n_reactions = n_reactions
         self.time = 0
         self.samples = []
         self.sampling_times = sampling_times # to check
         self.current_state = x0
-        self.parameters = parameters
+        self.propensities = propensities
+        self.stoich_mat = stoich_mat
 
-    def params_propensities(self):
-        # associate to each function its parameter
-        lambdas = self.CRN.propensities
-        set_parameters = np.vectorize(lambda f, params: (lambda x: f(params, x)), excluded=[1])
-        self.param_propensities = set_parameters(lambdas, self.parameters)
 
     def step(self):
-        self.params_propensities()
-        sampling_times = self.sampling_times
         while True:
             eval_propensities = np.vectorize(lambda f, x: f(x), excluded=[1], otypes=[np.ndarray])
-            lambdas = eval_propensities(self.param_propensities, self.current_state)
+            lambdas = eval_propensities(self.propensities, self.current_state)
             if np.size(lambdas) > 1:
                 lambdas = np.concatenate(lambdas)
-                lambdas.reshape(self.CRN.n_species, self.CRN.n_reactions)
+                lambdas.reshape(self.n_species, self.n_reactions)
             lambda0 = lambdas.sum()
             probabilities = np.cumsum(lambdas) / lambda0
             delta = np.random.exponential(1/lambda0)
@@ -50,39 +56,28 @@ class SSA:
             u = random.random()
             ind_reaction = np.searchsorted(probabilities, u, side='left') # the reaction n°ind_reaction occurs
             # updating state
-            self.current_state += self.CRN.stoichiometric_mat[:, ind_reaction]
+            self.current_state += self.stoich_mat[:, ind_reaction]
             # sampling if needed
-            last_index = int(np.searchsorted(sampling_times, self.time - delta, side='right'))
-            current_index = int(np.searchsorted(sampling_times, self.time, side='right'))
+            last_index = int(np.searchsorted(self.sampling_times, self.time - delta, side='right'))
+            current_index = int(np.searchsorted(self.sampling_times, self.time, side='right'))
             for _ in range(current_index - last_index):
                 self.samples.append(list(self.current_state))
         return self.sampling_times, self.samples
-    
-    def outputs(self):
-        n_samples = len(self.sampling_times)
-        n_params = len(self.parameters)
-        t = np.expand_dims(self.sampling_times, axis=0).transpose()
-        p = self.parameters * n_samples
-        p = np.reshape(p, (n_samples, n_params))
-        return np.concatenate((t,p), axis=1), self.samples
 
 
 # testing
      
-def lambda1(params, x):
-    return params[0]
+# def lambda1(params, x):
+#     return params[0]
 
-stoichiometric_mat = np.array([1]).reshape(1,1)
+# stoichiometric_mat = np.array([1]).reshape(1,1)
 
-crn = CRN(stoichiometric_mat, np.array([lambda1]))
+# crn = CRN(stoichiometric_mat, np.array([lambda1]), 1)
 
-sampling_times = np.linspace(0, 1, 50)
-ssa = SSA(np.array([0.]), 700, crn, sampling_times, [2.])
+# sampling_times = [5, 10, 25, 100]
 
-sampling_times, samples = ssa.step()
+# # print(crn.parameters_propensities(np.array([lambda1]), ))
 
-print(samples)
+# sampling_times, samples = crn.simulation(np.array([0.]), np.array([10.]), sampling_times, 100)
 
-X, y = ssa.outputs()
-
-print(y)
+# print(samples)
