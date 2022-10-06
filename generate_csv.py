58951ccc87ae33499823753c5aba8066dab4c048
gabrielleblj <gabrielleblj@gmail.com>
2022-10-06 17:35:32 +0200

    index correction

--- generate_csv.py
+++ generate_csv.py
@@ -7,14 +7,14 @@ from CRN2_birth_death import propensities_birth_death as propensities
 # because we use multiprocessing
 if __name__ == '__main__':
 
-    CRN_NAME = 'birth'
+    CRN_NAME = 'birth_death'
     datasets = {'train': 1280, 'valid': 128, 'test': 640}
     DATA_LENGTH = sum(datasets.values())
 
-    stoich_mat = propensities.stoich_mat.reshape(2, 1)
+    stoich_mat = propensities.stoich_mat.reshape(1, 2)
     crn = simulation.CRN(stoichiometric_mat= stoich_mat, propensities=np.array([propensities.lambda1, propensities.lambda2]), n_params=2)
     dataset = generate_data.CRN_Dataset(crn=crn, sampling_times=np.array([5, 10, 15, 20]))
-    X, y = dataset.generate_data(data_length=DATA_LENGTH, n_trajectories=10**4, sobol_length=2., initial_state=(True, np.ones(1)))
+    X, y = dataset.generate_data(data_length=DATA_LENGTH, n_trajectories=10**4, sobol_length=np.array([2., 1.]), initial_state=(True, np.ones(1)))
 
 
     # writing CSV files
